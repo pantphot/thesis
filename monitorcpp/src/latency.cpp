a2b1ec8b280a29bd252c156069287e57d33108a7
Pantelis <pantphotiou@gmail.com>
2019-01-10 22:18:51 +0200

    calculating msg_loss

--- monitorcpp/src/latency.cpp
+++ monitorcpp/src/latency.cpp
@@ -28,12 +28,16 @@ latency_min(),
 latency_max(0),
 latency_last(0),
 // frequency(),
-jitter(0)
+jitter(0),
+received_msg_id(0),
+msg_loss(0),
+current_msg(0)
 {
   // Initialize a subscriber that will receive the ROS Image message to be displayed.
   std::cerr << "Subscribing to topic ' " << topic << "'" << std::endl;
   sub = this->create_subscription<sensor_msgs::msg::Image>(
       topic.c_str(), std::bind(&CalculateStatistics::callback, this,  std::placeholders::_1),custom_qos_profile);
+  clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
 
 
 }
@@ -44,9 +48,21 @@ void CalculateStatistics::callback(const std::shared_ptr<sensor_msgs::msg::Image
     receive_msg(msg, this->get_logger());
 }
 
-void CalculateStatistics::sample(const rclcpp::Time time_received, const rclcpp::Time time_sent, rclcpp::Logger logger){
+void CalculateStatistics::receive_msg(const std::shared_ptr<sensor_msgs::msg::Image> msg,rclcpp::Logger logger)
+{
+  received_msg_id = std::stoi (msg->header.frame_id,nullptr,10);
+  RCLCPP_INFO(logger, "Received image #%d", received_msg_id);
+  // static int count = 0;
+  auto time_sent = rclcpp::Time( msg->header.stamp.sec, msg->header.stamp.nanosec,RCL_SYSTEM_TIME);
+  // rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+  auto time_received = clock->now();
+  sample(time_received,time_sent,received_msg_id,logger);
+}
+
+void CalculateStatistics::sample(const rclcpp::Time time_received, const rclcpp::Time time_sent, int received_msg_id, rclcpp::Logger logger){
   n_msgs_received+=1;
   //Compute Latency
+  auto begin_calc = clock->now();
   latency = time_received.nanoseconds() - time_sent.nanoseconds();
   latency =  RCL_NS_TO_MS(latency);
   latency_avg_last = latency_avg;
@@ -55,32 +71,36 @@ void CalculateStatistics::sample(const rclcpp::Time time_received, const rclcpp:
   if (n_msgs_received==1) latency_min = latency;
   latency_min = (latency < latency_min)?(latency):(latency_min);
   latency_max = (latency > latency_max)?(latency):(latency_max);
+
   // Interarrival jitter https://www.ietf.org/rfc/rfc3550.txt (mean deviation) packet length matters ,https://tools.ietf.org/html/rfc1889#appendix-A.8 example usage
   //          J(i) = J(i-1) + (|D(i-1,i)| - J(i-1))/16
-
   if (n_msgs_received > 1){
     auto d = latency - latency_last;
     latency_last = latency;
     if(d<0) d = -d;
     jitter +=  (double(d) - jitter)/16;// 16 noise reduction ratio
   }
-  RCLCPP_INFO(logger, "Latency %lf ms", latency);
-  RCLCPP_INFO(logger, "Latency_avg %lf ms", latency_avg);
+  // If best effort calculate message loss
+  if (custom_qos_profile.reliability == 2){
+    if (n_msgs_received == 1){
+      current_msg = received_msg_id;
+    }
+    else{
+      msg_loss += received_msg_id - current_msg - 1;
+      current_msg = received_msg_id;
+    }
+  }
+  auto done_calc = clock->now();
+  auto calc_time = done_calc.nanoseconds() - begin_calc.nanoseconds();
+  // RCLCPP_INFO(logger, "Time for calculations %ld ns", calc_time);
+  // RCLCPP_INFO(logger, "Latency %lf ms", latency);
+  // RCLCPP_INFO(logger, "Latency_avg %lf ms", latency_avg);
+  RCLCPP_INFO(logger, "Message Loss %d ",msg_loss);
   // RCLCPP_INFO(logger, "Latency_std %lf ", latency_std);
   // RCLCPP_INFO(logger, "Latency_min %lf ms", latency_min);
   // RCLCPP_INFO(logger, "Latency_max %lf ms", latency_max);
   // RCLCPP_INFO(logger, "Jitter %lf ", jitter);
 
-}
-
-void CalculateStatistics::receive_msg(const std::shared_ptr<sensor_msgs::msg::Image> msg,rclcpp::Logger logger)
-{
-  RCLCPP_INFO(logger, "Received image #%s", msg->header.frame_id.c_str());
-  // static int count = 0;
-  auto time_sent = rclcpp::Time( msg->header.stamp.sec, msg->header.stamp.nanosec,RCL_SYSTEM_TIME);
-  rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
-  auto time_received = clock->now();
-  sample(time_received,time_sent,logger);
 }
 
 int main(int argc, char * argv[])
