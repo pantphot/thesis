33ca3f47d595f0b872ae5244d99e8a54428a2617
ros2 <pantphotiou@gmail.com>
2019-03-04 12:40:13 +0200

    Removed eye detection

--- detection/src/detection.cpp
+++ detection/src/detection.cpp
@@ -6,33 +6,32 @@ using namespace std;
 using namespace cv;
 void detectAndDisplay( Mat frame );
 CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
+// CascadeClassifier eyes_cascade;
 int main(int argc, char * argv[])
 {
     CommandLineParser parser(argc, argv,
                              "{help h||}"
-                             "{face_cascade|../include/data/haarcascades/haarcascade_frontalface_alt.xml|Path to face cascade.}"
-                             "{eyes_cascade|../include/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml|Path to eyes cascade.}"
+                             // "{face_cascade|../include/data/haarcascades/haarcascade_frontalface_alt.xml|Path to face cascade.}"
+                             // "{eyes_cascade|../include/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml|Path to eyes cascade.}"
                              "{camera|0|Camera device number.}");
     parser.about( "\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
                   "You can use Haar or LBP features.\n\n" );
     parser.printMessage();
     // String face_cascade_name = parser.get<String>("face_cascade");
     // String eyes_cascade_name = parser.get<String>("eyes_cascade");
-    std::cout << argv[0] << '\n';
     auto face_cascade_name = "./src/detection/data/haarcascade_frontalface_alt.xml";
-    auto eyes_cascade_name = "./src/detection/data/haarcascade_eye_tree_eyeglasses.xml";
+    // auto eyes_cascade_name = "./src/detection/data/haarcascade_eye_tree_eyeglasses.xml";
     //-- 1. Load the cascades
     if( !face_cascade.load( face_cascade_name ) )
     {
         cout << "--(!)Error loading face cascade\n";
         // return -1;
     };
-    if( !eyes_cascade.load( eyes_cascade_name ) )
-    {
-        cout << "--(!)Error loading eyes cascade\n";
-        // return -1;
-    };
+    // if( !eyes_cascade.load( eyes_cascade_name ) )
+    // {
+    //     cout << "--(!)Error loading eyes cascade\n";
+    //     // return -1;
+    // };
     int camera_device = parser.get<int>("camera");
     VideoCapture capture;
     //-- 2. Read the video stream
@@ -73,14 +72,14 @@ void detectAndDisplay( Mat frame )
         ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4 );
         Mat faceROI = frame_gray( faces[i] );
         //-- In each face, detect eyes
-        std::vector<Rect> eyes;
-        eyes_cascade.detectMultiScale( faceROI, eyes );
-        for ( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4 );
-        }
+        // std::vector<Rect> eyes;
+        // eyes_cascade.detectMultiScale( faceROI, eyes );
+        // for ( size_t j = 0; j < eyes.size(); j++ )
+        // {
+        //     Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+        //     int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+        //     circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4 );
+        // }
     }
     //-- Show what you got
     imshow( "Capture - Face detection", frame );
